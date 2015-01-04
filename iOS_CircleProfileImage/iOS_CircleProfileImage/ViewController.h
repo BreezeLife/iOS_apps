1d2e6efe9036cbd1f8506c680c5d5a3e9629fe07
Breeze <breezelifepro@gmail.com>
2015-01-04 02:19:09 -0800

    add iOS Circle ProfileImage Project

---
+++ iOS_CircleProfileImage/iOS_CircleProfileImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS_CircleProfileImage
+//
+//  Created by Breeze Zhao on 1/4/15.
+//  Copyright (c) 2015 WeiqiZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS_CircleProfileImage/iOS_CircleProfileImageTests/iOS_CircleProfileImageTests.m
@@ -0,0 +1,40 @@
+//
+//  iOS_CircleProfileImageTests.m
+//  iOS_CircleProfileImageTests
+//
+//  Created by Breeze Zhao on 1/4/15.
+//  Copyright (c) 2015 WeiqiZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface iOS_CircleProfileImageTests : XCTestCase
+
+@end
+
+@implementation iOS_CircleProfileImageTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
