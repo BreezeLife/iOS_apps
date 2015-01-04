0c6a04a9219f1f8dac1ba3ee3c8dbda99166cdc4
Breeze <breezelifepro@gmail.com>
2015-01-04 05:03:15 -0800

    update CircleProfileImage

--- iOS_CircleProfileImage/iOS_CircleProfileImage/ViewController.m
+++ iOS_CircleProfileImage/iOS_CircleProfileImage/ViewController.m
@@ -17,6 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.ProfileImageView1.layer.cornerRadius = self.ProfileImageView1.frame.size.width / 4;
+    self.ProfileImageView1.clipsToBounds = YES;
+    
+    
+    
+    //for image 2
+    self.ProfileImageView2.layer.borderWidth = 3.0f;
+    self.ProfileImageView2.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
